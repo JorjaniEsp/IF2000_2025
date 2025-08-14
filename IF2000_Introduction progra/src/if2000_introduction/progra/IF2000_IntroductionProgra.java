33e87907b02d83f1e39f20b9d4fce6873ad62514
JorjaniEsp <espinozajorjanie2006@gmail.com>
2025-08-14 11:41:50 -0600

    Mi primer commit en Java

---
+++ IF2000_Introduction progra/src/if2000_introduction/progra/IF2000_IntroductionProgra.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package if2000_introduction.progra;
+
+/**
+ *
+ * @author josse
+ */
+public class IF2000_IntroductionProgra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String name = "Liones";
+        int age = 40;
+        
+        System.out.println("Hello Word");
+        System.out.println("Name" + name);
+        System.out.println("Age" + age);
+
+        
+    }
+    
+}
